7ea23517ff0dd80a29549cf1dac3589a60bbb023
Ezequiel <ezequielm789@gmail.com>
2022-05-10 09:51:19 -0300

    2nd option

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import requests
 import json
 
-search_title =["TV LG 49"]
+title_to_search= input("Titulo del producto a buscar \n")
+
+response_search = requests.get('https://api.mercadolibre.com/sites/MLA/search?q=' + title_to_search)
+item_id =  response_search.json()['results'][0]['id']
+item_search = requests.get('https://api.mercadolibre.com/items/' + item_id)
+out_file = open("myfile.json", "w")     
+json.dump(item_search.json(), out_file)        
+out_file.close() 
+
+
+""" search_title =["TV LG 49"]
 
 for x in search_title:
     response_search = requests.get('https://api.mercadolibre.com/sites/MLA/search?q=' + x)
@@ -11,4 +21,4 @@ for x in search_title:
     json.dump(item_search.json(), out_file)        
     out_file.close() 
 
-    
+     """
